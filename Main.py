ac09eaf4b4f3bed9bd6507826d174175f820351d
urk21cs1200 <109951889+urk21cs1200@users.noreply.github.com>
2022-09-06 23:26:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,52 @@
-# Read an integer that denotes the length of the list which is returned as the output of the algorithm
-length_of_circular_linked_list = int(input())
-# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
-circular_linked_list = list(map(int,input().strip().split(" ")))
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
-# 1 2 3 4 1 2 3 4 5 6
-new_list = []
-key = 0
-while len(new_list) < length_of_circular_linked_list and key < len(circular_linked_list):
-    element = circular_linked_list[key]
-    if element not in new_list:
-        new_list.append(element)
-    key += 1
-print(len(new_list))
-for i in new_list:
-    print(i , end = " ")
+
+class Queue:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def enqueue(self, data) -> None:
+    # Write your code here
+    new = Node(data)
+    if not self.tail is None:
+      self.tail.next = new
+    if self.head is None:
+      self.head = new
+    self.tail = new
+    
+
+  def dequeue(self) -> None:
+    # Write your code here
+    if not self.head is None:
+      self.head = self.head.next
+      if self.head is None:
+        self.tail = None
+
+
+  def status(self) -> None:
+    # Write your code here
+    elements = ""
+    curr = self.head
+    while not curr is None:
+      elements += str(curr.data) + "=>"
+      curr = curr.next
+    print(elements + "None")
+
+
+# Do not change the following code
+queue = Queue()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
